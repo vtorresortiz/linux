bf19cb1b04736fd8c6de7cbe1cb99101debd5605
Víctor Torres <vito.ubb@gmail.com>
2023-11-18 16:09:12 -0300

    Create xinit_touchpad.sh

---
+++ xinit_touchpad.sh
@@ -0,0 +1,8 @@
+# libinput Accel Custom Motion Points
+xinput set-prop 10 347 0, 1.4, 5, 7, 11, 15, 19
+
+# libinput Accel Custom Motion Step 
+xinput set-prop 10 348 0.75
+
+# libinput Accel Profile Enabled
+xinput set-prop 10 343 0 0 1
